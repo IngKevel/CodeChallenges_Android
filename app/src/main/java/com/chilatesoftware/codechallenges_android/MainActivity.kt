7861bf28f24aa83dd055cd535413be8575f32956
kevinvelez-wizeline <kevin.velez@wizeline.com>
2026-04-09 19:01:45 -0600

    first commit

---
+++ app/src/main/java/com/chilatesoftware/codechallenges_android/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.chilatesoftware.codechallenges_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.chilatesoftware.codechallenges_android.ui.theme.CodeChallenges_AndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CodeChallenges_AndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(
+        text = "Code Challenges - Android",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CodeChallenges_AndroidTheme {
+        Greeting()
+    }
+}
